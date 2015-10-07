8494bca48b5fd37e2061d0a7f740a477f399189e
Pseamars <patrick.seamars@gmail.com>
2015-10-07 17:53:12 -0600

    trying some fun with local storage => fail

--- habit/js/habitApp.js
+++ habit/js/habitApp.js
@@ -4,14 +4,36 @@ var $Calendar = $('section#Calendar');
 var n = 0;
 var colorDone = '#68B567';
 var colorPending = '#B56767';
+var days = [];
 
 // var dayPiece = '<section class="dayPiece pending ' + n +' "></section>';
 
-for (var i = 0; i <= 30; i++){
-	$Calendar.append('<section id="' + n +' " class="dayPiece pending "></section>');
-	n++;
-	console.log(n);
-}
+var getItems = function(sItem){
+	 window.localStorage.getItem(sItem);
+	 return JSON.parse(sItem);
+};
+
+var setItems = function(sName, sValue){
+
+	var lsValue = JSON.stringify(sValue);
+	 window.localStorage.setItems(sName, lsValue);
+};
+
+
+var loadDays = function(){
+	for (var i = 0; i <= 30; i++){
+		if(!getItems(days)){
+			$Calendar.append('<section id="' + n +' " class="dayPiece pending "></section>');
+			var day = new DayObject(n);
+			setItems('days', day);
+			n++;
+		} else {
+			console.log(getItems(days)); // for now
+		}
+	}
+};
+
+console.log(getItems(days));
 
 var $dayPiece = $('section.dayPiece');
 
@@ -22,4 +44,29 @@ $dayPiece.on('click', function () {
 
 	console.log('You got me');
 });
+
+
+var getItems = function(sItem){
+	 window.localStorage.getItem(sItem);
+};
+
+var setItems = function(sName, sValue){
+
+}
+
+function DayObject(habitId, habitName, habitDuration) {
+	this.habitID =  habitId | '';
+	this.habitStatus =  "pending";
+	this.habitName = habitName | '';
+	this.habitDuration =  habitDuration | '';
+	this.talk = function(){
+		console.log(message);
+	};
+
+};
+
+var dayOne = new DayObject('Crushing it', 'Winning');
+
+console.log(dayOne.habitStatus);
+
 
